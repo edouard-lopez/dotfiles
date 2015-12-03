a0265d703d1781d2b675ab1971a4c17f1b3efad5
Edouard Lopez <dev+git@edouard-lopez.com>
2015-12-03 15:09:36 +0100

    add basic support to fish with tmux

---
+++ config.fish
@@ -0,0 +1,10 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /home/elopez/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /home/elopez/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+source ~/.my-fishrc
