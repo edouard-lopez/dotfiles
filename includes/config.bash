388010568037d3da5decaabb28e317816247a89a
Édouard Lopez <dev+git@edouard-lopez.com>
2016-09-13 22:44:49 +0200

    move *.bash to includes/posix/, add $BASE_INCLUDE and $POSIX_INCLUDE

--- includes/config.bash
+++ includes/config.bash
@@ -4,10 +4,10 @@ function configure() {
     function refresh() { source "$HOME"/.bashrc; }
 
     function bash-sensible() {
-        if [[ ! -f ./sensible.bash ]]; then
+        if [[ ! -f "$BASE_INCLUDE/sensible.bash" ]]; then
             curl -o sensible.bash -L https://raw.githubusercontent.com/mrzool/bash-sensible/master/sensible.bash
         fi
-        source ./sensible.bash
+        source "$BASE_INCLUDE/sensible.bash"
     }
     bash-sensible
 }
