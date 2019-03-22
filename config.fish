c13f147b71686300806ef13c152aa61cadf483b7
Edouard Lopez <contact@edouard-lopez.com>
2019-03-22 14:21:21 +0100

    configure edouard-lopez/pure for fish

--- config.fish
+++ config.fish
@@ -3,3 +3,4 @@ source ~/.my-fishrc
 
 # THEME PURE #
 set fish_function_path /home/ed8/.config/fish/functions/theme-pure/functions/ $fish_function_path
+set --global --export PURE_EXECUTABLE_PATH $HOME/.pure/
