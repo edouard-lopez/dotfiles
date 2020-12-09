cf787aa068e92e58ac093699e8c6f2db5828abcd
Edouard Lopez <contact@edouard-lopez.com>
2020-12-09 16:09:12 +0100

    remove tackle and tacklebox

--- config.fish
+++ config.fish
@@ -4,23 +4,3 @@ source ~/.my-fishrc
 # THEME PURE #
 set fish_function_path /home/ed8/.config/fish/functions/theme-pure/functions/ $fish_function_path
 set --global --export PURE_EXECUTABLE_PATH $HOME/.pure/
-
-
-
-# Paths to your tackle
-set tacklebox_path ~/.tackle ~/.tacklebox
-
-# Theme
-#set tacklebox_theme entropy
-
-# Which modules would you like to load? (modules can be found in ~/.tackle/modules/*)
-# Custom modules may be added to ~/.tacklebox/modules/
-# Example format: set tacklebox_modules virtualfish virtualhooks
-
-# Which plugins would you like to enable? (plugins can be found in ~/.tackle/plugins/*)
-# Custom plugins may be added to ~/.tacklebox/plugins/
-# Example format: set tacklebox_plugins python extract
-
-# Load Tacklebox configuration
-. ~/.tacklebox/tacklebox.fish
-
