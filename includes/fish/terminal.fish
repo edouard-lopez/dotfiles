062ecda510ec7fdcb8dfb4a88a1204e78e755340
Edouard Lopez <contact@edouard-lopez.com>
2020-12-31 02:52:02 +0100

    drop konsole-related theme change methods

--- includes/fish/terminal.fish
+++ includes/fish/terminal.fish
@@ -4,37 +4,6 @@ set -gx EDITOR vim
 set -gx TERM screen-256color # support 256 color palette
 set -gx VISUAL vim
 
-function change-terminal-color-mode
-    set arg "$argv[1]"
-    or set arg "colors=base16-tomorrow"
-    if test -z "$TMUX"
-        and type konsoleprofile >/dev/null
-        konsoleprofile "$arg"
-    else
-        printf '\033Ptmux;\033\033]50;%s\007\033\\ ' "$arg"
-    end
-end
-
-# Theme mode
-function night-mode
-    tmux source-file $HOME/dotfiles/.tmux/themes/tomorrow-night-bright.tmux
-    tmux source-file $HOME/.tmux.conf
-    change-terminal-color-mode "colors=base16-tomorrow-night"
-
-end
-
-function day-mode
-    tmux source-file $HOME/dotfiles/.tmux/themes/tomorrow.tmux
-    tmux source-file $HOME/.tmux.conf
-    change-terminal-color-mode "colors=base16-tomorrow"
-
-end
-
-# so we can distinguish between Yakuake and Konsole (no tmux)
-function konsole
-    TERMINAL_CONTEXT='konsole' command konsole
-end
-
 function start_tmux
     # https://wiki.archlinux.org/index.php/Tmux#Start_tmux_on_every_shell_login
     if type tmux >/dev/null
