2eb23a3d7f0720fd82e90a6da73503b558d8abbc
Édouard Lopez <contact@edouard-lopez.com>
2023-02-07 11:41:31 +0100

    fix(hightlight): fallback to `less -R` when highlihgting manpage and --help
    highlight: Undefined language definition. Use --syntax option.

--- includes/fish/pager.fish
+++ includes/fish/pager.fish
@@ -1,12 +1,18 @@
 # PAGER / LESS
 #set --export --universal PAGER most
+set EMPTY ""
 
 function less \
-    --description "syntax higlighting for the Pager, see http://www.andre-simon.de/"
+    --description "syntax higlighting for the Pager, see http://www.andre-simon.de/" \
+    --argument-names file_to_highlight
 
-    highlight \
-        --style base16/one-light \
-        --out-format=xterm256 \
-        $argv[1] \
-    | command less -R
+    if test -n "$file_to_highlight"
+        highlight \
+            --style base16/one-light \
+            --out-format=xterm256 \
+            "$file_to_highlight" \
+            | command less -R
+    else
+        command less -R
+    end
 end
