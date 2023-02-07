477626b8993a9c7c57e9253a572aaff36396c0c1
Édouard Lopez <contact@edouard-lopez.com>
2023-02-07 11:39:26 +0100

    feat(PNPM): declare PNPM_* related variables

--- config.fish
+++ config.fish
@@ -4,6 +4,11 @@ source ~/.my-fishrc
 # THEME PURE #
 set fish_function_path /home/ed8/.config/fish/functions/theme-pure/functions/ $fish_function_path
 set --global --export PURE_EXECUTABLE_PATH $HOME/.pure/
+
+# pnpm
+set -gx PNPM_HOME "/home/edouard/.local/share/pnpm"
+set --append --export --global PATH $PNPM_HOME
+# pnpm end
 set --global --export VOLTA_HOME $HOME/.volta
 set --append --export --global PATH $VOLTA_HOME/bin
 set --global --export PNPM_HOME $HOME/.local/share/pnpm
