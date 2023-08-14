5d62bf22532c89870d3b55cfca4c00ce9c117619
Édouard Lopez <contact@edouard-lopez.com>
2023-08-14 15:56:05 +0200

    refactor: use fish_add_path for krew binary, PNPM_HOME and VOLTA_HOME

--- config.fish
+++ config.fish
@@ -4,16 +4,16 @@ source ~/.my-fishrc
 # THEME PURE #
 set fish_function_path /home/ed8/.config/fish/functions/theme-pure/functions/ $fish_function_path
 set --global --export PURE_EXECUTABLE_PATH $HOME/.pure/
-set -gx VOLTA_HOME $HOME/.volta
-fish_add_path --append PATH $VOLTA_HOME/bin
 set --global --export VOLTA_HOME $HOME/.volta
+fish_add_path --append $VOLTA_HOME/bin
 
 # pnpm
 set -gx PNPM_HOME /home/ed8/.local/share/pnpm
-set --append --export --global PATH $PNPM_HOME
+fish_add_path --append $PNPM_HOME
 # pnpm end
 source $HOME/.asdf/asdf.fish
 
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+fish_add_path --append $HOME/.krew/bin
