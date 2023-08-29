734985a284c82359f5fa4485a001aba041eae78c
Édouard Lopez <contact@edouard-lopez.com>
2023-08-29 22:09:10 +0200

    feat: add backup rsync commands

--- includes/fish/backup.fish
+++ includes/fish/backup.fish
@@ -2,5 +2,7 @@
 # @param: void
 # @return: stdout
 function rsync2external-disk
-    crontab -l | grep 'rsync' | sed 's!@daily !!' | while read -r cmd; bash -c "$cmd"; end
+    #    crontab -l | grep 'rsync' | sed 's!@daily !!' | while read -r cmd; bash -c "$cmd"; end
+    rsync -r -t -p -o -g -v --progress --size-only -u -l -H -s /data/Pictures /media/ed8/ed-big5/data-backup/
+    rsync -r -t -p -o -g -v --progress --size-only -u -l -H -s /data/paperwork /media/ed8/ed-big5/data-backup/
 end
