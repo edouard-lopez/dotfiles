f7feedbd57388817e9763739b18c0f996e01c3b3
Édouard Lopez <contact@edouard-lopez.com>
2023-11-13 15:04:40 +0100

    chore: correct typo

--- includes/config.bash
+++ includes/config.bash
@@ -14,4 +14,4 @@ export HISTFILESIZE=10000        # increase history file size (default is 500)
 export HISTSIZE=${HISTFILESIZE}  # increase history size (default is 500)
 export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"   # mem/file sync
 # if this is interactive shell, then bind hh to Ctrl-r (for Vi mode check doc)
-if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hh \C-j"'; fi
+# if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hh \C-j"'; fi
