c52308d5d21ca3d0ed04de2bd40dd730335d80bf
Édouard Lopez <contact@edouard-lopez.com>
2026-02-05 21:54:21 +0100

    refactor(pdf): improve compress-pdf command to take quality argument

--- includes/fish/functions/compress-pdf.fish
+++ includes/fish/functions/compress-pdf.fish
@@ -2,7 +2,8 @@ function compress-pdf \
     --description 'Compress a PDF file by converting it to Djvu and back to PDF. outputFile name is optional' \
     --argument-names inputFile \
     --argument-names outputFile \
-    --argument-names dpi
+    --argument-names dpi \
+    --argument-names quality
 
     if not test -f "$inputFile"
         echo "❌ Input file not found: $inputFile"
@@ -10,8 +11,8 @@ function compress-pdf \
     end
 
     set djvuFile (string replace '.pdf' '.djvu' $inputFile)    
-    set --query $dpi[1]; or set dpi 300 # smaller dpi means smaller file size but lower quality
-    pdf2djvu --dpi=$dpi --output="$djvuFile" "$inputFile"
+    set --query dpi[1]; or set dpi 300 # smaller dpi means smaller file size but lower quality
+    pdf2djvu --dpi=$dpi --output="$djvuFile" "$inputFile" --verbose
     if test $status -eq 0
         echo "✅ Converted to Djvu"
     else
@@ -19,8 +20,8 @@ function compress-pdf \
         return 1
     end
 
-    set --query $outputFile[1]; or set outputFile (string replace '.pdf' '.compressed.pdf' $inputFile)
-    set --query $quality[1]; or set quality 75 # smaller quality means smaller file size but lower quality
+    set --query outputFile[1]; or set outputFile (string replace '.pdf' '.compressed.pdf' $inputFile)
+    set --query quality[1]; or set quality 75 # smaller quality means smaller file size but lower quality
     ddjvu -verbose -format=pdf -quality=$quality "$djvuFile" "$outputFile"
     if test $status -eq 0
         echo "✅ Compressed back to PDF"
